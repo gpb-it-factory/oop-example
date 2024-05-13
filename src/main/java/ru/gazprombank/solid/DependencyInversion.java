6a5b937847f51d8a045dc747d169ed358fa3ced6
Vaganov Vadim <vrvaganov@gmail.com>
2024-05-13 09:06:23 +0300

    Добавлены примеры по ISP и DIP

--- src/main/java/ru/gazprombank/solid/DependencyInversion.java
+++ src/main/java/ru/gazprombank/solid/DependencyInversion.java
@@ -1,48 +1,66 @@
 import java.math.BigDecimal;
 
-///
-
 public void main() {
+    var shop = new Shop(new Cash());
+    shop.doPayment(new Order(), BigDecimal.valueOf(300));
+
+    var shopDIP = new ShopDIP(new CashDIP());
+    shopDIP.doPayment(new Order(), BigDecimal.valueOf(400));
+    var shopCifRub = new ShopDIP(new CifRub());
+    shopCifRub.doPayment(new Order(), BigDecimal.valueOf(400));
+    var shopCard = new ShopDIP(new BankCard());
+    shopCard.doPayment(new Order(), BigDecimal.valueOf(500));
 }
 
 public class Cash {
-    public void doTransaction(BigDecimal amount){
-        //logic
+    public void doTransaction(BigDecimal amount) {
+        System.out.println(STR."Совершена транзакция наличными на сумму \{amount}");
     }
 }
 
 public class Shop {
-    private Cash cash;
+    private final Cash cash;
     public Shop(Cash cash) {
         this.cash = cash;
     }
-    public void doPayment(Object order, BigDecimal amount){
+    public void doPayment(Order order, BigDecimal amount){
         cash.doTransaction(amount);
     }
 }
 
 ///
 
-
-public interface Payments {
+public interface PaymentMethod {
     void doTransaction(BigDecimal amount);
 }
 
-public class Cash2 implements Payments {
-    @Override
-    public void doTransaction(BigDecimal amount) {
-        //logic
+public class ShopDIP {
+    private final PaymentMethod paymentMethod;
+    public ShopDIP(PaymentMethod paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+    public void doPayment(Order order, BigDecimal amount){
+        paymentMethod.doTransaction(amount);
     }
 }
-public class BankCard implements Payments{
+
+public class CashDIP implements PaymentMethod {
     @Override
     public void doTransaction(BigDecimal amount) {
-        //logic
+        System.out.println(STR."Совершена транзакция наличными на сумму \{amount}");
     }
 }
-public class PayByPhone implements Payments {
+public class BankCard implements PaymentMethod {
     @Override
     public void doTransaction(BigDecimal amount) {
-        //logic
+        System.out.println(STR."Совершена транзакция с карты на сумму \{amount}");
     }
 }
+public class CifRub implements PaymentMethod {
+    @Override
+    public void doTransaction(BigDecimal amount) {
+        System.out.println(STR."Совершена транзакция цифровым рублём на сумму \{amount}");
+    }
+}
+
+public class Order {}
